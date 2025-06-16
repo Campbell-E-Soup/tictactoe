0b51f1bce4a12b7787c7a2e938c69ba0776cd0f3
campbell-e-soup <campbellchudley@gmail.com>
2025-06-16 17:59:57 -0400

    lint fix

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -12,6 +12,6 @@ class HomeControllerTest < ActionController::TestCase
     end
   end
 
-  #yes i wrote a test for place but do not want to ever deal with the session during testing and it took so long I never want to set the expectation that I would ever do that again, so no there is no test
+  # yes i wrote a test for place but do not want to ever deal with the session during testing and it took so long I never want to set the expectation that I would ever do that again, so no there is no test
 
 end
