49e88232d940fbedf28f814ef204a01c49056a32
campbell-e-soup <campbellchudley@gmail.com>
2025-06-16 18:00:41 -0400

    lint fix

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -5,11 +5,6 @@ class HomeControllerTest < ActionController::TestCase
     get :index
     assert_response :success
   end
-  module TTTSessionStub
-    def ttt_session
-      @ttt_session_stub
-    end
-  end
 
   # yes i wrote a test for place but do not want to ever deal with the session during testing and it took so long I never want to set the expectation that I would ever do that again, so no there is no test
 
